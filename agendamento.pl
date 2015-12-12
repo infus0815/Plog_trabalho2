d16f436a91540aceba5d3170017fe4bef0e54689
Alexandre Lima <infus@sapo.pt>
2015-12-12 21:37:39 +0000

    time constraint working + maximizing people per room

--- agendamento.pl
+++ agendamento.pl
@@ -30,6 +30,8 @@ reuniaoprefteste1([1,2,3],[3,5,5]).
 salasteste1([10,10,20,10,10,20]-[1,1,2,1,1,2]).
 
 
+
+%%%%%%%%%%%%UTILS%%%%%%%%%%%%%%
 getMaxValue(R_ids,Value) :- 
 	maximum(Value,R_ids).
 
@@ -45,9 +47,21 @@ getTotalSize([Sala1|T],Size) :-
 count_equals(_,[],0).
 count_equals(V,[H|T],C) :- 
 	count_equals(V,T,C1),
-	H #= V #<=> C2,
+	(H #= V) #<=> C2,
 	C #= C1 + C2.
-	
+
+flatten(List, FlatList) :-
+ flatten(List, [], FlatList0), !,
+ FlatList = FlatList0.
+
+ flatten(Var, Tl, [Var|Tl]) :- var(Var), !.
+ flatten([], Tl, Tl) :- !.
+ flatten([Hd|Tl], Tail, List) :- !,
+     flatten(Hd, FlatHeadTail, List),
+     flatten(Tl, Tail, FlatHeadTail).
+flatten(NonList, Tl, [NonList|Tl]).
+
+%%%%%%%%%inits%%%%%%%%%%%%%
 createDuration([],[]).
 createDuration([[Duration,_,_,_]|TReunioesPretendidas],D) :- 
 	createDuration(TReunioesPretendidas,DT),
@@ -64,6 +78,8 @@ createMachines([_Hsala|Tsala],L,Count) :-
 	createMachines(Tsala,Ltemp,Count1),
 	append([machine(Count,1)],Ltemp,L).
 	
+	
+%%%%%%%%%%%%%tests%%%%%%%%%%%%%
 teste :-
 	reuniao1(A),
 	reuniao2(B),
@@ -73,7 +89,9 @@ teste :-
 	salasteste1(S),
 	agendamento(L,S).	
 
-	
+
+
+%%%%%%%%%%%%%%%%CODE%%%%%%%%%	
 agendamento(ReunioesPretendidas,SalasCap-SalasCar) :- 
 	length(ReunioesPretendidas,Rsize),
 	length(SS,Rsize),
@@ -85,25 +103,31 @@ agendamento(ReunioesPretendidas,SalasCap-SalasCar) :-
 	createMachines(SalasCap,Machines,1),
 	
 	
-	domain(SS,1,100),
-	domain(D,1,100),
-	domain(ES,1,100),
+	domain(SS,1,1000),
+	domain(D,1,1000),
+	domain(ES,1,1000),
 	domain(SalasReuniao,1,Rsize),
-	domain([End],1,100),
+	domain([End],1,1000),
 	maximum(End,ES),
 	
 	assign_salas_reuniao(SalasReuniao,ReunioesPretendidas,SalasCap-SalasCar),
 	assign_reuniao_participantes(Reunioes,ReunioesPretendidas,SalasReuniao,SalasCap,CountOpt),
-	
+	time_constraint(Reunioes,ReunioesPretendidas,SS,ES),
 	
 	cumulatives(ReunioesTask,Machines,[bound(upper)]),
 	
 	
 	
 	append(SS,[End],Vars1),
-	append(Vars1,SalasReuniao,Vars),
-	labeling([],Vars),
-	write(SS),nl,write(End),nl,write(SalasReuniao).
+	append(Vars1,SalasReuniao,Vars2),
+	append(Vars2,Reunioes,Vars3),
+	append(Vars3,CountOpt,Vars4),
+	
+	flatten(Vars4,Vars),
+	
+	
+	labeling([maximize(CountOpt)],Vars),
+	write(SS),nl,write(ES),nl,write(SalasReuniao),nl,write(Reunioes),nl,write(ReunioesPretendidas),nl,write(CountOpt).
 	
 
 assign_salas_reuniao(_,[],_).
@@ -162,9 +186,51 @@ assign_reuniao_participantes([HReunioes|TReunioes],[ [_,MO,MOD,MP] |TReunioesPre
 	
 	TotalOpt #= CountOpt1 + CountOpt2.
 	
+flatten_reunioes_pretendidas([_,MO,MOD,MP],Result) :- 
+	append(MO,MOD,R1),
+	append(R1,MP,Result).
+	
+
+%count = 0 if intercept and 1 if no interception
+check_intercept_reunioes(SS1,ES1,SS2,ES2,Count) :- 
+	(SS1 #>= ES2) #<=> C1, %Reuniao2 antes de Reuniao1
+	(SS2 #>= ES1) #<=> C2, %Reuniao1 antes de Reuniao2
+	Count #= C1 + C2.
+	
+
+count_equals2(_,_,[],_,0).
+count_equals2(V,B,[H1|T1],[H2|T2],C) :- 
+	count_equals2(V,B,T1,T2,C1),
+	(H1 #= V) #<=> C2,
+	C3 #= B * H2 * C2,
+	C #= C1 + C3.
+	
+		
+%returns number of same people	
+count_equals3([],_,_,_,0).
+count_equals3([HV|TV],[HB|TB],RP2,R2,C) :-
+	count_equals3(TV,TB,RP2,R2,C1),
+	count_equals2(HV,HB,RP2,R2,C2),
+	C #= C1 + C2.
 	
 	
-/*avoid_same_time(SS,ES,Result) :- */
+
+%evita mesmas pessoas em duas reunioes ao mesmo tempo
+avoid_same_time(_Reuniao,_ReuniaoPret,_SS,_ES,[],[],[],[]).
+avoid_same_time(Reuniao,ReuniaoPret,SS,ES,[HReunioes|TReunioes],[HReunioesPretendidas|TReunioesPretendidas],[HSS|TSS],[HES|TES]) :- 
+	check_intercept_reunioes(SS,ES,HSS,HES,C1),
+	flatten_reunioes_pretendidas(ReuniaoPret,ReuniaoPret1),
+	flatten_reunioes_pretendidas(HReunioesPretendidas,ReuniaoPret2),
+	count_equals3(ReuniaoPret1,Reuniao,ReuniaoPret2,HReunioes,C2),
+	C1 #= 1 #\/ C2 #= 0,
+	avoid_same_time(Reuniao,ReuniaoPret,SS,ES,TReunioes,TReunioesPretendidas,TSS,TES).
+
+time_constraint([],[],[],[]).
+time_constraint([HReunioes|TReunioes],[HReunioesPretendidas|TReunioesPretendidas],[HSS|TSS],[HES|TES]) :- 
+	avoid_same_time(HReunioes,HReunioesPretendidas,HSS,HES,TReunioes,TReunioesPretendidas,TSS,TES),
+	time_constraint(TReunioes,TReunioesPretendidas,TSS,TES).
+	
+	
 	
         
         
